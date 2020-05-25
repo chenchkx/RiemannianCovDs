c9726a74b4e95241a26ad49b819cac371b82793c
Kai-Xuan Chen <36260120+Kai-Xuan@users.noreply.github.com>
2020-05-25 14:08:51 +0800

    del

--- tool_kit/Jeff_Divergence_Here.m
+++
@@ -1,9 +0,0 @@
-function outS = Jeff_Divergence_Here(current_StructuralDes,current_m1,cluster_center,center_m1)
-    n = size(current_StructuralDes,1);
-    t = 0.5 * trace(center_m1 * current_StructuralDes) + 0.5 * trace(cluster_center* current_m1) - n;
-    outS = t;
-
-    if  (outS < 1e-10)
-        outS= 0.0;
-    end
-end
